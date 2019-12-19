d1ec3b53dda941e9ee877e4922c1f1c699e9de2f
Kyle Cheung <kcheu4@uwo.ca>
2019-12-19 18:08:30 -0500

    added post request for register object

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -3,7 +3,26 @@ form.addEventListener('submit', handleRegister);
 
 async function handleRegister(event) {
   // stop paging from refreshing
-  console.log(form.elements.username.value);
   event.preventDefault();
-  console.log('test');
+  const obj = form.elements;
+
+  // TODO add validation
+  if (obj.password.value !== obj.passwordConfirm.value) {
+    console.log('passwords dont match');
+    return;
+  }
+
+  const response = await fetch('/api/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: obj.username.value,
+      password: obj.password.value,
+      email: obj.email.value,
+      firstName: obj.firstName.value,
+      lastName: obj.lastName.value,
+    }),
+  });
 }
