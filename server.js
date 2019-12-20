12a7222ca3a5b4852c4aae80f6602f05898404e9
Kyle Cheung <kcheu4@uwo.ca>
2019-12-20 16:24:41 -0500

    refactor main server code. removed extra imports

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const express = require('express');
 const path = require('path');
 const registerRoute = require('./routes/registerAuth');
 const loginRoute = require('./routes/loginAuth');
-const db = require('./db');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-const insertQuery = 'INSERT INTO users(id, user_name, password, first_name, last_name, email) VALUES (gen_random_uuid(),$1, $2, $3, $4, $5)';
 
 // CREATE TABLE users (
 //   id UUID,
@@ -21,15 +19,6 @@ const insertQuery = 'INSERT INTO users(id, user_name, password, first_name, last
 //   last_name VARCHAR(50) NOT NULL,
 //   email varchar(100) NOT NULL UNIQUE
 //   );
-
-// db.query(insertQuery, ['g2s', '6666', '999', 'kyle', 'cheung', 'g2s@gmail.com'], (err, res) => {
-//   if (err) {
-//     console.log('there was an error');
-//   } else {
-//     const user = res.rows[0];
-//     console.log(res.rowCount);
-//   }
-// });
 
 // serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
